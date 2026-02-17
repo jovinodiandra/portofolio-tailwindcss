7bec96da0cea4717e31d12b87e882210b3793c03
jovinodiandra <jovinodiandra230@gmail.com>
2026-02-17 22:34:26 +0700

    enambahkan fitur baru dark mode

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-
-
 //navbar Fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
@@ -20,3 +18,40 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// klik di luar halaman
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//dark Mode
+const toggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+toggle.addEventListener("change", function () {
+  if (toggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+
+// cek apakah toggle nya di mode yg sesuai
+if (
+  document.documentElement.classList.toggle(
+    "dark",
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches),
+  )
+) {
+  toggle.checked = true;
+} else {
+  toggle.checked = false;
+}
